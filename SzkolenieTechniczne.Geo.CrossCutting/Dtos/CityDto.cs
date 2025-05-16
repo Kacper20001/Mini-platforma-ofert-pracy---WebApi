e281cb01996b41d000412320cd0e1a0a1fdf3839
Kacper Kulig <kuligkacper20001@gmail.com>
2025-05-16 19:18:12 +0200

    Dodanie klasy DTO: CountryDto i CityDto dla modułu Geo

---
+++ SzkolenieTechniczne.Geo.CrossCutting/Dtos/CityDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SzkolenieTechniczne.CommonCrossCutting.Dtos;
+using SzkolenieTechniczne.CommonCrossCutting.ValidationAttributes;
+
+namespace SzkolenieTechniczne.Geo.CrossCutting.Dtos
+{
+    public class CityDto
+    {
+        public Guid Id { get; set; }
+
+        [LocalizedStringRequired]
+        [LocalizedStringLength(64)]
+        public LocalizedString Name { get; set; }
+
+        [Required]
+        public Guid CountryId { get; set; }
+
+    }
+}
