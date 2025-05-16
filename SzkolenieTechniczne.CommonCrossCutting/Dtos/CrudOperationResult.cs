681202767cf8926448acff5e8ae3893ab370282f
kulig <kulig@HP_KACPER>
2025-05-16 17:24:45 +0200

    Dodaj pliki projektów.

---
+++ SzkolenieTechniczne.CommonCrossCutting/Dtos/CrudOperationResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzkolenieTechniczne.CommonCrossCutting.Dtos
+{
+    public class CrudOperationResult<Tdto>
+    {
+        public CrudOperationResultStatus Status { get; set; }
+        public Tdto? Result { get; set; }
+    }
+}
