53dbcd00ce087c93ccfd307c1c07992c7657c414
Kacper Kulig <kuligkacper20001@gmail.com>
2025-05-16 22:11:40 +0200

    Implementacja katalogu Services (CityService, CountryService)

--- SzkolenieTechniczne.Geo/Services/CityService.cs
+++ SzkolenieTechniczne.Geo/Services/CityService.cs
@@ -1,6 +1,59 @@
-﻿namespace SzkolenieTechniczne.Geo.Services
+﻿using Microsoft.EntityFrameworkCore;
+using SzkoeleieTechniczne.Geo.Storage;
+using SzkoeleieTechniczne.Geo.Storage.Entities;
+using SzkolenieTechniczne.CommonCrossCutting.Dtos;
+using SzkolenieTechniczne.Geo.CrossCutting.Dtos;
+using SzkolenieTechniczne.Geo.Extensions;
+
+namespace SzkolenieTechniczne.Geo.Services
 {
     public class CityService
     {
+        private GeoDbContext _geoDbcontext;
+        public CityService(GeoDbContext geoDbcontext)
+        {
+            _geoDbcontext = geoDbcontext;
+        }
+
+        public async Task<CityDto> GetById(Guid id)
+        {
+            var city = await _geoDbcontext
+                .Set<City>()
+                .Include(x => x.Translations)
+                .AsNoTracking()
+                .Where(e => e.Id!.Equals(id))
+                .SingleOrDefaultAsync();
+            
+            return city.ToDto();
+        }
+        
+        public async Task<IEnumerable<CityDto>> Get()
+        {
+            var cities = await _geoDbcontext
+                .Set<City>()
+                .Include(x => x.Translations)
+                .AsNoTracking()
+                .Select(e => e.ToDto())
+                .ToListAsync();
+            
+            return cities;
+        }
+        public async Task<CrudOperationResult<CityDto>>Create(CityDto dto)
+        {
+            var entity = dto.ToEntity();
+
+            _geoDbcontext
+                .Set<City>()
+                .Add(entity);
+
+            await _geoDbcontext.SaveChangesAsync();
+            var newDto = await GetById(entity.Id);
+
+            return new CrudOperationResult<CityDto>
+            {
+                Result = newDto,
+                Status = CrudOperationResultStatus.Success
+            };
+        }
     }
 }
--- SzkolenieTechniczne.Geo/Services/CountryService.cs
+++ SzkolenieTechniczne.Geo/Services/CountryService.cs
@@ -1,6 +1,58 @@
-﻿namespace SzkolenieTechniczne.Geo.Services
+﻿using Microsoft.EntityFrameworkCore;
+using SzkoeleieTechniczne.Geo.Storage;
+using SzkoeleieTechniczne.Geo.Storage.Entities;
+using SzkolenieTechniczne.CommonCrossCutting.Dtos;
+using SzkolenieTechniczne.Geo.CrossCutting.Dtos;
+using SzkolenieTechniczne.Geo.Extensions;
+
+namespace SzkolenieTechniczne.Geo.Services
 {
     public class CountryService
     {
+        private GeoDbContext _geoDbcontext;
+        public CountryService(GeoDbContext geoDbcontext)
+        {
+            _geoDbcontext = geoDbcontext;
+        }
+        public async Task<CountryDto> GetById(Guid id)
+        {
+            var country = await _geoDbcontext
+                .Set<Country>()
+                .Include(x => x.Translations)
+                .AsNoTracking()
+                .Where(e => e.Id!.Equals(id))
+                .SingleOrDefaultAsync();
+
+            return country.ToDto();
+        }
+        public async Task<IEnumerable<CountryDto>> Get()
+        {
+            var cities = await _geoDbcontext
+                .Set<Country>()
+                .Include(x => x.Translations)
+                .AsNoTracking()
+                .Select(e => e.ToDto())
+                .ToListAsync();
+            return cities;
+        }
+
+        public async Task<CrudOperationResult<CountryDto>> Create(CountryDto dto)
+        {
+            var entity = dto.ToEntity();
+
+            _geoDbcontext
+                .Set<Country>()
+                .Add(entity);
+
+            await _geoDbcontext.SaveChangesAsync();
+
+            var newDto = await GetById(entity.Id);
+
+            return new CrudOperationResult<CountryDto>
+            {
+                Result = newDto,
+                Status = CrudOperationResultStatus.Success
+            };
+        }
     }
 }
