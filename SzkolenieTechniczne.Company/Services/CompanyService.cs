fc68845b28ef19f47711272df985e265c0854cf9
Kacper Kulig <kuligkacper20001@gmail.com>
2025-06-14 12:35:42 +0200

    Dodanie serwisów CompanyService i JobPositionService oraz rejestracja DI

--- SzkolenieTechniczne.Company/Services/CompanyService.cs
+++ SzkolenieTechniczne.Company/Services/CompanyService.cs
@@ -1,6 +1,24 @@
-﻿namespace SzkolenieTechniczne.Company.Services
+﻿using SzkolenieTechniczne.Company.CrossCutting.Dtos;
+using SzkolenieTechniczne.Company.Extensions;
+using SzkolenieTechniczne.Company.Storage;
+
+
+namespace SzkolenieTechniczne.Company.Services
 {
     public class CompanyService
     {
+        private readonly CompanyDbContext _context;
+
+        public CompanyService(CompanyDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<CompanyDto> GetAll()
+        {
+            return _context.Companies
+                .Select(c => c.ToDto())
+                .ToList();
+        }
     }
 }
--- SzkolenieTechniczne.Company/Services/JobPositionService.cs
+++ SzkolenieTechniczne.Company/Services/JobPositionService.cs
@@ -1,6 +1,23 @@
-﻿namespace SzkolenieTechniczne.Company.Services
+﻿using SzkolenieTechniczne.Company.CrossCutting.Dtos;
+using SzkolenieTechniczne.Company.Extensions;
+using SzkolenieTechniczne.Company.Storage;
+
+namespace SzkolenieTechniczne.Company.Services
 {
     public class JobPositionService
     {
+        private readonly CompanyDbContext _context;
+
+        public JobPositionService(CompanyDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<JobPositionDto> GetAll()
+        {
+            return _context.JobPositions
+                .Select(j => j.ToDto())
+                .ToList();
+        }
     }
 }
